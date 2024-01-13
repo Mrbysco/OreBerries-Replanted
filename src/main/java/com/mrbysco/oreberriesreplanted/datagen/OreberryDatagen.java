7dde2ba786558d2152809065ea9f67de8243cafa
Mrbysco <Mrbysco@users.noreply.github.com>
2024-01-13 16:01:11 +0100

    Fix the vat recipes having berries as result instead of input

--- src/main/java/com/mrbysco/oreberriesreplanted/datagen/OreberryDatagen.java
+++ src/main/java/com/mrbysco/oreberriesreplanted/datagen/OreberryDatagen.java
@@ -510,7 +510,7 @@ public class OreberryDatagen {
 					.save(tagOutput,
 							new ResourceLocation(Reference.MOD_ID, type + "_from_smelting"));
 
-			VatRecipeBuilder.vat(Ingredient.of(berry), new ResourceLocation(Reference.MOD_ID, type + "_oreberry_juice"), Ingredient.of(nuggetTag))
+			VatRecipeBuilder.vat(Ingredient.of(nuggetTag), new ResourceLocation(Reference.MOD_ID, type + "_oreberry_juice"), Ingredient.of(berry))
 					.unlockedBy("has_berry", has(berry))
 					.save(tagOutput,
 							new ResourceLocation(Reference.MOD_ID, "vat/" + type + "_nugget"));
