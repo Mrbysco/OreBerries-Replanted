93faf3ac887a5c470eb4e002e899b8b8d37e0165
Mrbysco <Mrbysco@users.noreply.github.com>
2024-10-16 19:11:00 +0200

    Remove unused import

--- src/main/java/com/mrbysco/oreberriesreplanted/item/TooltipBlockItem.java
+++ src/main/java/com/mrbysco/oreberriesreplanted/item/TooltipBlockItem.java
@@ -8,7 +8,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
